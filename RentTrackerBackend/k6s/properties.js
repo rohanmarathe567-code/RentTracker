ac7b693d34316ce30cfc5ebb8332d6881aae644e
Rohan Marathe <rohan.marathe567@gmail.com>
2025-03-31 21:29:02 +1100

    k6 properties with faker

--- RentTrackerBackend/k6s/properties.js
+++ RentTrackerBackend/k6s/properties.js
@@ -1,25 +1,22 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { faker } from '@faker-js/faker';
+import faker from "k6/x/faker"
 
 export const options = {
-  stages: [
-    { duration: '5s', target: 5 },
-    { duration: '10s', target: 10 },
-    { duration: '5s', target: 0 },
-  ],
+  vus: 1,
+  iterations: 1
 };
 
 export default function () {
   // Create Property Test
   const createPropertyPayload = {
-    address: faker.location.streetAddress(),
-    city: faker.location.city(),
-    state: faker.location.state(),
-    zipCode: faker.location.zipCode(),
-    bedrooms: faker.number.int({ min: 1, max: 5 }),
-    bathrooms: faker.number.int({ min: 1, max: 3 }),
-    monthlyRent: faker.number.float({ min: 1000, max: 5000, precision: 0.01 }),
+    address: faker.address.street(),
+    city: faker.address.city(),
+    state: faker.address.state(),
+    zipCode: faker.address.zip(),
+    bedrooms: faker.numbers.intRange(1,5),
+    bathrooms: faker.numbers.intRange(1,5),
+    monthlyRent: faker.payment.price(0,1000),
   };
 
   const createRes = http.post('http://localhost:7000/api/properties', JSON.stringify(createPropertyPayload), {
---
+++ RentTrackerBackend/k6s/test.js
@@ -0,0 +1,15 @@
+﻿import faker from "k6/x/faker"
+
+export default function() {
+    const createPropertyPayload = {
+        address: faker.address.street(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zip(),
+        bedrooms: faker.numbers.intRange(1,5),
+        bathrooms: faker.numbers.intRange(1,5),
+        monthlyRent: faker.payment.price(0,1000),
+    };
+
+    console.log(createPropertyPayload)
+}
