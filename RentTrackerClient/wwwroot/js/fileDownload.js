bce67e663bf27d4f6ab08f66929401f239d510e9
Rohan Marathe <rohan.marathe567@gmail.com>
2025-04-06 17:11:09 +1000

    Fixed the minor inconsistencies with Attachment UI
    1. Fix the styling
    2. Fix the download functionality

---
+++ RentTrackerClient/wwwroot/js/fileDownload.js
@@ -0,0 +1,16 @@
+window.initializeFileDownload = () => {
+    window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+        const arrayBuffer = await contentStreamReference.arrayBuffer();
+        const blob = new Blob([arrayBuffer]);
+        const url = URL.createObjectURL(blob);
+        const anchorElement = document.createElement('a');
+        anchorElement.href = url;
+        anchorElement.download = fileName ?? '';
+        anchorElement.click();
+        anchorElement.remove();
+        URL.revokeObjectURL(url);
+    }
+};
+
+// Initialize when the script loads
+window.initializeFileDownload();
